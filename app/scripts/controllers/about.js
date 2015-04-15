8cb1093420ec031d9d98323e1ab9d9361758a0f3
Vinnie Magro <vmagro@socaldevs.com>
2015-04-14 19:35:52 -0700

    Initial commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name auvDashboardApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the auvDashboardApp
+ */
+angular.module('auvDashboardApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name auvDashboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the auvDashboardApp
+ */
+angular.module('auvDashboardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
